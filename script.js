db63d736665d530fb2e55d00e388422fe4804fcd
Mila Rut <mila.balashova@gmail.com>
2024-02-11 16:15:40 +0300

    add responsive styles

--- script.js
+++ script.js
@@ -78,6 +78,20 @@ document.addEventListener('DOMContentLoaded', function () {
       loop: false,
       slidesPerView: '3',
       spaceBetween: 20,
+      breakpoints: {
+        360: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        }
+      },
     });
   };
 
